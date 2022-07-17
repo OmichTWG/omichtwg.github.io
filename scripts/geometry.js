0ec5c28c5a5f1a2af1b9fab1b2c9a030592e79e1
OmichTWG <77875232+OmichTWG@users.noreply.github.com>
2022-07-18 01:25:06 +0300

    Delete scripts directory

--- scripts/geometry.js
+++
@@ -1,4 +0,0 @@
-const angleByDirection = (direction) => {
-    const reverse = direction.x < 0 ? 180 : 0;
-    return reverse + Math.atan(direction.y / direction.x) / Math.PI * 180;
-}
--- scripts/sounds.js
+++
@@ -1,7 +0,0 @@
-const playSoundWM = (name, volume = 1, loop = false) => {
-    return playSound(name, volume * (global.music / 5), loop);
-}
-
-const playSoundWV = (name, volume = 1, loop = false) => {
-    return playSound(name, volume * (global.volume / 5), loop);
-}
